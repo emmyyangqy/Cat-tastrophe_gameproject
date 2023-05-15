f3568f8debc2bd9c9121428b896342e4088d1426
emmyyangqy <91351762+emmyyangqy@users.noreply.github.com>
2023-05-15 16:55:48 -0700

    completed cup breaking animation

--- Cup_1.gd
+++ Cup_1.gd
@@ -1,9 +1,9 @@
 extends pushableobject
 #class_name pushableobject
 
-enum State {DEFAULT, BREAK}
+enum State {DEFAULT, BREAK, BROKEN}
 
-var gravity = 2000
+var gravity = 100
 var friction = 0.5
 var fallingtime = 0
 var curstate = State.DEFAULT
@@ -19,23 +19,32 @@ func switch_to(new_state: State):
 		$AnimatedSprite2D.play("default")
 	elif new_state == State.BREAK:
 		$AnimatedSprite2D.play("break")
+		
+	elif new_state == State.BROKEN:
+		$AnimatedSprite2D.play("broken")
+		$CollisionShape2D.queue_free()
 
 func _physics_process(delta):
 	
-	velocity.y += gravity
+	if !is_on_floor() and curstate != State.BROKEN:
+		velocity.y += gravity
 	
 	
 	if !is_on_floor():
-		pass
+		fallingtime += delta
+	if fallingtime > 0.2 and is_on_floor():
+		velocity.x = velocity.x* 0.5
+		switch_to(State.BREAK)
+		fallingtime = 0
 	
+	if is_on_floor() and fallingtime < 0.2:
+		fallingtime = 0
 	
 	
 	move_and_slide()
 	velocity.x = velocity.x* 0.93
 	#velocity = Vector2.ZERO
 	
-	
-
 
 func pushrightkinematic():
 	velocity.x += 100
@@ -44,3 +53,8 @@ func pushleftkinematic():
 	velocity.x += -100
 	
 	
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if curstate == State.BREAK:
+		switch_to(State.BROKEN)
