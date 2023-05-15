c89d0c3dfd7738db906c68664ff04b362e56b0fe
emmyyangqy <91351762+emmyyangqy@users.noreply.github.com>
2023-05-15 09:39:43 -0700

    working to add current state label above character for debugging purposes

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -13,20 +13,16 @@ const MOVE_STATES = [State.WALK_RIGHT, State.WALK_LEFT, State.RUN_RIGHT, State.R
 
 var lastvelocity = 0
 var curstate = State.IDLE
-var state_time = 0 
-var death_time = 0
-var next = null
-var hitnum = 0
-var last_velocity = 0
+
 var jump_num = 0
 var gravity = 2000
 var jump_force = 650
 var run_speed = 400
 var walk_speed = 150
 var jump_done = 0
-var isInLight = false
 var laststate = State.IDLE
 var previousstate = State.IDLE
+
 #var raycast = get_parent().get_node("LightArea/RayCast2D")
 var raycast 
 var LightArea
@@ -38,34 +34,22 @@ var camera
 	
 #
 func _ready():
-	#var raycast = get_parent().get_node("Camera/LightArea/RayCast2D")
-		# Set the "Cast To" property to the position of the player node
-	#var player = get_node(".") # Change this to match the path of your player node
-	#print(player.global_position)
-	#raycast.cast_to = player.global_position #gtehridjosesrgouhifjsdk
-
-	switch_to(State.IDLE)
 	
-#func _ready() -> void:
-#	get_parent().print_tree()
+	switch_to(State.IDLE)
 	
 
 func switch_to(new_state: State):
 	
-#	if curstate == State.death and death_time > 3:
-#		new_state = State.revive
-	
-	
+#
 	if curstate == State.JUMP:
 		
 		if is_on_floor():
-#			$cat_animation.play("jump_land")
-#			$cat_animation.flip_h = false
+
 			if lastvelocity < 0:
 				#$cat_animation.frame = 0
 				$cat_animation.play("jump_land")
 				$cat_animation.flip_h = true
-			elif lastvelocity > 0:
+			elif lastvelocity >= 0:
 				#$cat_animation.frame = 0
 				$cat_animation.play("jump_land")
 				$cat_animation.flip_h = false
@@ -73,8 +57,7 @@ func switch_to(new_state: State):
 				#$cat_animation.frame = 0
 				#print("error")
 				pass
-				#$cat_animation.play("jump_land")
-			#wait(1)
+
 			await get_tree().create_timer(.1).timeout
 			pass
 			
@@ -84,15 +67,9 @@ func switch_to(new_state: State):
 		else:
 			return
 			
-	
-#
-#	if curstate == State.death and death_time > 3:
-#		new_state = State.revive
-#
-		
+
 	curstate = new_state
-	state_time = 0.0
-	
+
 
 	if new_state == State.IDLE:
 		#$cat_animation.play("idle_2")
@@ -132,7 +109,6 @@ func switch_to(new_state: State):
 		$cat_animation.flip_h = true
 		
 	elif new_state == State.JUMP:
-		#if is_on_floor() or jump_num < 3:
 		if is_on_floor() and jump_num < 1:
 			jump_num += 1
 			
@@ -159,7 +135,6 @@ func switch_to(new_state: State):
 #				velocity.y = -jump_force
 #				if Input.is_action_pressed("ui_right"):
 #					velocity.x = last_velocity 
-
 #			if is_on_floor():
 #				$cat_animation.play("jump_land")
 #				$cat_animation.flip_h = false
@@ -178,55 +153,30 @@ func switch_to(new_state: State):
 		$cat_animation.play("sleep")
 	elif new_state == State.SCARED:
 		$cat_animation.play("scared")
+		
 	
-		
-		
-#func hit():
-#	hitnum += 1
-#
-#	if hitnum > 3:
-#		switch_to(State.dying)
-#	else:
-#		switch_to(State.hit)
+	
 
 
 func _physics_process(delta):
-	# Update the amount of time you spent in the current state
-	state_time += delta
-	
-	#var player = get_node(".")
-	
-	
-#	var raycast = get_parent().get_node("LightArea/RayCast2D")
-#		# Set the "Cast To" property to the position of the player node
-#	var player = get_node(".") # Change this to match the path of your player node
-#	#print(player.global_position)
-#	raycast.target_position = player.global_position #gtehridjosesrgouhifjsdk
-	#print(curstate)
-	#print(player.global_position)
-	
 
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-
 	if Input.is_action_just_pressed("jump"):
 		jump_done = 0
 		switch_to(State.JUMP)
-		
 
 	elif Input.is_action_pressed("walk_left"):
 		if Input.is_action_pressed("shift"):
 			switch_to(State.RUN_LEFT)
 		else: switch_to(State.WALK_LEFT)
 		
-		
 	elif Input.is_action_pressed("walk_right"):
 		if Input.is_action_pressed("shift"):
 			switch_to(State.RUN_RIGHT)
 		else: switch_to(State.WALK_RIGHT)
-		
 		
 	elif Input.is_action_pressed("paw"):
 		switch_to(State.PAW)
@@ -234,7 +184,6 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("sleep"):
 		switch_to(State.SLEEP)
 	
-
 	else:
 		switch_to(State.IDLE)
 		
@@ -250,8 +199,7 @@ func _physics_process(delta):
 				#print("ghjrebksldaGDSJHK")
 			else:
 				velocity.x = -walk_speed
-#
-			
+
 #			if laststate == 3:
 #				velocity.x = -run_speed
 #			else:
@@ -298,6 +246,8 @@ func _physics_process(delta):
 		if curstate != State.JUMP:
 			laststate = curstate
 			
+	#Global.state_num = curstate
+			
 	raycast = get_parent().get_node("Camera/RayCast2D")
 	LightArea = get_parent().get_node("Camera/LightArea")
 	camera = get_parent().get_node("Camera")
@@ -310,74 +260,26 @@ func _physics_process(delta):
 			get_tree().reload_current_scene()
 		else: 
 			pass
+			
+	for index in get_slide_collision_count():
+		var collision = get_slide_collision(index)
+		if collision.get_collider is pushableobject:
+			collision.get_collider.slide(-collision.normal*(run_speed/2))
+			
 
-
-
-	
 
 func _on_animated_sprite_2d_animation_finished():
 	
-#	if curstate == State.dying:
-#		switch_to(State.death)
-#		death_time = 0 
-		
 	if curstate == State.PAW:
 		switch_to(State.IDLE)
-		
-	if curstate == State.JUMP:  #switch to elif
+#
+	elif curstate == State.JUMP:  #switch to elif
 		switch_to(State.IDLE)
-		
-#	elif curstate == State.revive:
-#		switch_to(MOVE_STATES.pick_random())
-#		hitnum = 0
-	
 
 
 func _on_light_area_body_entered(body):
 	if body.get_name() == "Player":
 		dangerzone = true
-#	if caught == true:
-#		get_tree().reload_current_scene()
-	#var raycast = get_parent().get_node("LightArea/RayCast2D")
-	
-	#if body.name == "Area2D":
-	#	print("test")
-		#var light_shape = body.get_node("CollisionShape2D") as CollisionShape2D
-		#var light = body.get_parent().get_node("PointLight2D")
-	#var raycast = body.get_node("RayCast2D")
-#	var raycast = get_parent().get_node("Camera/LightArea/RayCast2D")
-#	var player = get_node(".") # Change this to match the path of your player node
-	#print(player.global_position)
-	
-	
-#	if raycast.is_colliding():
-#		#pass
-#		print(raycast.get_collider().name)
-		#print(raycast.global_position, ' ', raycast.target_position, ' ', raycast.position, ' ', player.position)
-
-		
-#	if raycast.is_colliding() and raycast.get_collider().name == ".":
-#		print("bhjrksnd") # Restart the game or level
-#		get_tree().reload_current_scene()	
-			
-		#if light.is_raycast_enabled():
-			#get_tree().reload_current_scene()
-			
-
-			
-			#if light_shape.test_point(global_transform.origin):
-				# Restart the game or level
-				#get_tree().reload_current_scene()
-			
-
-
-func _draw():
-
-	#draw_line(Vector2(0,0), raycast.position-player.position, Color.RED)
-	
-	#draw_line(player.position, player.position+raycast.position, Color.WHITE)
-	pass
-
 
 func _on_light_area_body_exited(body):
 	dangerzone = false  # Replace with function body.
---
+++ Cup_1.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+class_name pushableobject
+
+var gravity = 2000
+
+
+func _physics_process(delta):
+	
+	velocity.y += gravity
+	move_and_slide()
+	velocity = Vector2.ZERO
+	
+func slide(vector):
+	velocity.x = vector.x
